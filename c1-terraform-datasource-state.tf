56927a8156cafbdbd0ae9f296e2fac485cbc2119
Michael Orlov <34108460+harleylrn@users.noreply.github.com>
2022-04-04 14:11:29 -0400

    Update c1-terraform-datasource-state.tf

--- c1-terraform-datasource-state.tf
+++ c1-terraform-datasource-state.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "project1" {
     # backend = "azurerm"
     config = {
         resource_group_name = "terraform-storage-rg"
-        storage_account_name = "terraformstate203"
-        container_name = "tfstatefiles"
-        key = "network-terraform.tfstate"
+        storage_account_name = "michaelostatestorage"
+        container_name = "tfstate"
+        key = "tf/tf.tfstate"
   }
 }
